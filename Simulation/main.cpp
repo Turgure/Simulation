4614b2ce360224f3f54d58d916dd169fa332acf7
TANNPOSP <yuutae07@gmail.com>
2012-12-17 20:45:59 +0900

    test
    fjalfjlajlfajljflajldasjl

--- Simulation/main.cpp
+++ Simulation/main.cpp
@@ -2,6 +2,10 @@
 #include "Keyboard.h"
 #include "BattleScene.h"
 
+//test
+//test git共有テスト
+
+
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
 	// ウインドウモードに変更
 	ChangeWindowMode(true);
