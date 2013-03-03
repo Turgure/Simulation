62aec4363c68a55d91ecc1d7be9bd0cc4fd593b7
Turgure <toranana_bye@yahoo.co.jp>
2013-03-04 02:52:30 +0900

    MoveByPx（）の編集

--- Simulation/Position.cpp
+++ Simulation/Position.cpp
@@ -7,22 +7,22 @@ Position::Position(int x, int y){
 	this->x = x, this->y = y;
 }
 
-void Position::setXByPx(int toX){ x = Stage::getLeftupPositionX()+ toX*mapsize; }
-void Position::setYByPx(int toY){ y = Stage::getLeftupPositionY()+ toY*mapsize; }
-int Position::getXByPx() const { return Stage::getLeftupPositionX()+ x*mapsize; }
-int Position::getYByPx() const { return Stage::getLeftupPositionY()+ y*mapsize; }
+void Position::setXByPx(int toX){ x = Stage::getLeftupPositionX() + toX*mapsize; }
+void Position::setYByPx(int toY){ y = Stage::getLeftupPositionY() + toY*mapsize; }
+int Position::getXByPx() const { return Stage::getLeftupPositionX() + x*mapsize; }
+int Position::getYByPx() const { return Stage::getLeftupPositionY() + y*mapsize; }
 void Position::setXByMap(int toX){ x = toX; }
 void Position::setYByMap(int toY){ y = toY; }
 int Position::getXByMap() const { return x; }
 int Position::getYByMap() const { return y; }
 
 void Position::setByPx(int toX, int toY){
-	x = Stage::getLeftupPositionX()+ toX*mapsize;
-	y = Stage::getLeftupPositionY()+ toY*mapsize;
+	x = Stage::getLeftupPositionX() + toX*mapsize;
+	y = Stage::getLeftupPositionY() + toY*mapsize;
 }
 void Position::MoveByPx(int diffX, int diffY){
-	x += Stage::getLeftupPositionX()+ diffX*mapsize;
-	y += Stage::getLeftupPositionY()+ diffY*mapsize;
+	x += diffX*mapsize;
+	y += diffY*mapsize;
 }
 void Position::setByMap(int toX, int toY){
 	x = toX, y = toY;
